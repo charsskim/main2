2e17601bcfea2fad48ebcce8c0aa1af8804e0b64
charsskim <mcprince73@naver.com>
2023-12-15 11:58:54 +0900

    메인 뉴스1완성

--- src/component/main/Main.jsx
+++ src/component/main/Main.jsx
@@ -1,9 +1,11 @@
+import News from './News';
 import Visual from './Visual';
 
 export default function Main() {
 	return (
 		<div className='Main'>
 			<Visual />
+			<News />
 		</div>
 	);
 }
---
+++ src/component/main/News.jsx
@@ -0,0 +1,28 @@
+export default function News() {
+	return (
+		<div className='News'>
+			<article>
+				<h1>Lorem, ipsum dolor.</h1>
+				<p>
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium minima fuga magni
+					nulla minus excepturi mollitia aperiam nobis ipsa officia?
+				</p>
+			</article>
+			<article>
+				<img src='/img/news1.jpg' alt='abc' />
+			</article>
+			<article>
+				<img src='/img/news2.jpg' alt='abc' />
+				<h2>lorem</h2>
+			</article>
+			<article>
+				<img src='/img/new3.jpg' alt='abc' />
+				<h2>Lorem, ipsum.</h2>
+			</article>
+			<article>
+				<img src='/img/new4.jpg' alt='abc' />
+				<h2>lorem</h2>
+			</article>
+		</div>
+	);
+}
